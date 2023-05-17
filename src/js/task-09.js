83004ffe6e361a48c9d6419f6369e855fff83e9c
Alinapikalova <alina.pikalova@gmail.com>
2023-05-17 14:00:02 +0200

    update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,7 +1,15 @@
-const 
+const colorChanging = document.querySelector(".widget");
+const nameColor = document.querySelector(".color");
+const colorChangingBtn = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
+colorChangingBtn.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  nameColor.textContent = color;
+  document.body.style.backgroundColor = color;
+});
